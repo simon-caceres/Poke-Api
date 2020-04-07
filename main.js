7891a5c02f2662345874466e5e998c060e0d97a8
simon caceres <kcrs2194@gmail.com>
2020-04-07 12:59:02 -0400

    Patrones de Busqueda

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
      $('#pokenumero').keyup(function(){
       var nombres = $('.card-title');
       var buscando = $(this).val();
+      
       var item='';
       for( var i = 0; i < nombres.length; i++ ){
           item = $(nombres[i]).html().toLowerCase();
@@ -32,32 +33,125 @@ $(document).ready(function(){
                     $(nombres[i]).parents('.card').hide();
                }
            }
-      }
+      }  
    });
+   $('.btn').on('click', function(){
+    let Dato = $('#pokenumero').val();
+    $('.Imprimir').html('');
+    async function AsyncSearch(){
+      try
+      {
+          //se realiza la primera peticion a la api de pokemons
+          
+          let response_1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${Dato}`);
+          let pokemon_1  = await response_1.json();
+  
+         
+          let respuesta = 
+          `<div class="col mb-4">
+          <div class="card bg-dark">
+          <div class="card-body">
+            <h5 class="card-title Pokename"> Nombre: ${pokemon_1.name}</h5>
+            <img src="${pokemon_1.sprites.front_default}" class="card-img-top pokeImg" alt="...">
+            <p class="card-text Pokename2">Habilidad: ${pokemon_1.abilities[0].ability.name}</p>
+          </div>
+        </div>
+        </div>`
+  
+          document.getElementById("Imprimir").innerHTML += respuesta;
+    }
+      catch(Error)
+      {
+          console.error(Error);
+          document.getElementById("Imprimir").innerHTML = Error;
+      }
+    
+  }
+  AsyncSearch()
+  });
+   
 });
 
-let page = 0;
+let page = 1;
 let large = 20;
 
 document.getElementById('Next').addEventListener('click', function () {
   page += 20
   large += 20
   $('.Imprimir').html('');
-  for(let index = `${page}` ; index <= `${large}`; index++) {
-    const Numero = index;
-  $.ajax({
-    url: `https://pokeapi.co/api/v2/pokemon/${Numero}`,
-     }).done(function (data) {
-    
-    $(".Imprimir").append(`<div class="col mb-4">
-    <div class="card bg-dark">
-    <div class="card-body">
-      <h5 class="card-title Pokename"> Nombre: ${data.name}</h5>
-      <img src="${data.sprites.front_default}" class="card-img-top pokeImg" alt="...">
-      <p class="card-text Pokename2">Habilidad: ${data.abilities[0].ability.name}</p>
-    </div>
-  </div>
-  </div>`);   
-});  
-};
+  async function AsyncNext(){
+    try
+    {
+        //se realiza la primera peticion a la api de pokemons
+        for(let index = `${page}` ; index <= `${large}`; index++) {
+          const Numero = index;
+        let response_1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${Numero}`);
+        let pokemon_1  = await response_1.json();
+
+       
+        let respuesta = 
+        `<div class="col mb-4">
+        <div class="card bg-dark">
+        <div class="card-body">
+          <h5 class="card-title Pokename"> Nombre: ${pokemon_1.name}</h5>
+          <img src="${pokemon_1.sprites.front_default}" class="card-img-top pokeImg" alt="...">
+          <p class="card-text Pokename2">Habilidad: ${pokemon_1.abilities[0].ability.name}</p>
+        </div>
+      </div>
+      </div>`
+
+        document.getElementById("Imprimir").innerHTML += respuesta;
+
+    }
+  }
+    catch(Error)
+    {
+        console.error(Error);
+        document.getElementById("Imprimir").innerHTML = Error;
+    }
+  
+}
+AsyncNext()
 });
+
+document.getElementById('Previous').addEventListener('click', function () {
+  page -= 20
+  large -= 20
+  $('.Imprimir').html('');
+    async function AsyncPrev(){
+        try
+        {
+            //se realiza la primera peticion a la api de pokemons
+            for(let index = `${page}` ; index <= `${large}`; index++) {
+              const Numero = index;
+            let response_1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${Numero}`);
+            let pokemon_1  = await response_1.json();
+    
+           
+            let respuesta = 
+            `<div class="col mb-4">
+            <div class="card bg-dark">
+            <div class="card-body">
+              <h5 class="card-title Pokename"> Nombre: ${pokemon_1.name}</h5>
+              <img src="${pokemon_1.sprites.front_default}" class="card-img-top pokeImg" alt="...">
+              <p class="card-text Pokename2">Habilidad: ${pokemon_1.abilities[0].ability.name}</p>
+            </div>
+          </div>
+          </div>`
+    
+            document.getElementById("Imprimir").innerHTML += respuesta;
+    
+        }
+      }
+        catch(Error)
+        {
+            console.error(Error);
+            document.getElementById("Imprimir").innerHTML = Error;
+        }
+      
+    }
+    AsyncPrev()
+});
+
+
+
